9db8a0b2ee54ae5295c88d5d66f10080e5551c70
sw347 <>
2024-03-19 10:31:36 +0900

    lastest

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const __dirname = path.resolve();
 const app = express();
 const port = 7321;
 
-// app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/index.html"));
+  res.sendFile(path.join("/index.html"));
 });
 
 app.listen(port, () => {
