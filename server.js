0bb18e0d22fcf4f19bfdfbcb798be85f1cf2a99f
sw347 <>
2024-03-19 09:50:43 +0900

    123

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import path from "path";
+const __dirname = path.resolve();
+const app = express();
+const port = 7321;
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+// app.get("/", (req, res) => {
+//   res.sendFile("/spark.js");
+// });
+
+app.listen(port, () => {
+  console.log(`server is on... localhost:${port}`);
+});
